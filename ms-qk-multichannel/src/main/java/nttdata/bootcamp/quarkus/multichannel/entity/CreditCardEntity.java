289ac096c7458a69d4e8d97fcc7d4d1d77590df4
LUIS1MB <luis.mendivil1b@gmail.com>
2023-02-20 17:13:12 -0500

    Actualizando postman

--- ms-qk-multichannel/src/main/java/nttdata/bootcamp/quarkus/multichannel/entity/CreditCardEntity.java
+++ ms-qk-multichannel/src/main/java/nttdata/bootcamp/quarkus/multichannel/entity/CreditCardEntity.java
@@ -1,5 +1,25 @@
 package nttdata.bootcamp.quarkus.multichannel.entity;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreditCardEntity {
+	
+    private Long idCreditCard;
+    private String descripcion;
+    private Double creditLimit;
+    private Double balanceAvailable;
+    private String creditCardNumber;
+    private Integer cvv;
+    private Date expirationDate;
+    private String closingDate;
+    private String lastOfPay;
+    private Client client;
 
 }
